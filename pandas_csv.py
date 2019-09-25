c8d8b7b60f4eb16c4bd5742459675d99dacc29f3
moses-mugoya <mosesmugoya31@gmail.com>
2019-09-25 07:25:17 +0300

    first commit

---
+++ pandas_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import datacompy
+df = pd.read_csv("mock_data.csv")
+males = 0
+females = 0
+cd = df[df['gender']!= "Male"]
+cde = df['gender']
+for c in cde:
+    if c == "Male":
+        males += 1
+    else:
+        females += 1
+
+print("The males are "+str(males))
+print("The females are "+str(females))
+
+
